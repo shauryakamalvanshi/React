e3c908457749b157132c4396b2c6c0caadf609ca
Shaurya Kamalvanshi <122744383+shauryakamalvanshi@users.noreply.github.com>
2023-06-17 22:29:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+const generateJokes = () => {
+    const setHeader = {
+        headers: {
+            Accept: "application/json"
+        }
+    };
+
+    fetch('https://icanhazdadjoke.com', setHeader)
+        .then((res) => res.json())
+        .then((data) => {
+            jokes.innerHTML = data.joke;
+        })
+        .catch((err) => {
+            console.log("Error:", err);
+        });
+};
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
+
+
+
